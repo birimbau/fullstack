67c42757b1eb040236ec2d0f145ab4edaeb70dc2
birimbau <tiagompbneto@gmail.com>
2020-04-11 17:42:18 +0200

    Fixed 0 index in parts array

--- part1/src/index.js
+++ part1/src/index.js
@@ -16,9 +16,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
       <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-      <Part part={props.parts[3].name} exercises={props.parts[3].exercises} />
     </div>
   );
 };
@@ -27,9 +27,9 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{' '}
-      {props.parts[1].exercises +
-        props.parts[2].exercises +
-        props.parts[3].exercises}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
     </p>
   );
 };
