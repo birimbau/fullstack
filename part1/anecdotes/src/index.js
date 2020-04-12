ea069f9b72faa49fd66f3938ecc8fa371fe75b52
birimbau <tiagompbneto@gmail.com>
2020-04-12 18:20:43 +0200

    1.14*: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -4,31 +4,40 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState(
-    Array.apply(null, new Array(10)).map(Number.prototype.valueOf, 0)
+    Array.apply(null, new Array(anecdotes.length)).map(
+      Number.prototype.valueOf,
+      0
+    )
   );
+  const max = Math.max(...points);
+  const maxIndex = points.findIndex((x) => x === max);
 
   const pickAnecote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   };
 
   const vote = () => {
-    const copy = { ...points };
+    const copy = [...points];
     copy[selected] += 1;
     setPoints(copy);
   };
-
+  console.log('Hey hey', Math.max.apply(null, points));
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {props.anecdotes[selected]}
       <br />
       <p>Has {points[selected]} points</p>
-
       <button type='button' onClick={vote}>
         Vote
       </button>
       <button type='button' onClick={pickAnecote}>
         Next anecdote
       </button>
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[maxIndex]}
+      <br />
+      <p>Has {max} votes</p>
     </div>
   );
 };
